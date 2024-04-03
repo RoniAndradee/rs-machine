e386f915bf5f9993df7f3940684a834dd4d37112
roniandradee <ronielli.santos@sptech.school>
2024-04-03 13:51:29 -0300

    criando o conversor de texto

---
+++ RS-conversor/script.js
@@ -0,0 +1,8 @@
+function converter(){
+    var texto = document.getElementById("inputTexto").value;
+    var conversao = document.getElementById("divResultado");
+    conversao.innerHTML = "";
+    for (var i = 0; i < texto.length; i++){
+      conversao.innerHTML += texto[i].charCodeAt(0).toString(2) + " ";
+    }
+  }
